66cc7b924ee7462e9c167409baa9a32f5fb8587f
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-16 00:50:19 -0500

    victara: add motorola-specific play store props and permissions
    Change-Id: I6ffcf0514d84d9be002d841ebf6ab5fbef041e9f
    
    Conflicts:
    	cm.mk

--- victara.mk
+++ victara.mk
@@ -3,5 +3,7 @@ $(call inherit-product, device/motorola/victara/full_victara.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := pa_victara
