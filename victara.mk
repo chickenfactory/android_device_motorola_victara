08f9fb64c43e47d65895d3cdca93d4a01acdd843
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-19 08:31:28 -0500

    init: set proper models and fingerprints for variants
    Change-Id: Ib813329157c564309e0369b415c1a5903590e0ca
    
    Conflicts:
    	victara.mk

--- victara.mk
+++ victara.mk
@@ -2,8 +2,3 @@ $(call inherit-product, device/motorola/victara/full_victara.mk)
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := pa_victara
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=victara_tmo \
-    BUILD_FINGERPRINT=motorola/victara_tmo/victara:5.0/LXE22.46-11/10:user/release-keys \
-    PRIVATE_BUILD_DESC="victara_tmo-user 5.0 LXE22.46-11 10 release-keys"
