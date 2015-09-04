7aa4de21f688d430791f10cea84633ba48f8b376
chickenfactory <micah.chickenfactory@gmail.com>
2015-09-03 17:19:39 -1000

    overlay: select ipv6 apn by default on t-mobile
    * also ensure overlay overrides anything in vendor/cm
    
    Change-Id: Ib306de657607f0d2e28bbdf82deb3d0845a1b2ba

--- victara.mk
+++ victara.mk
@@ -1,4 +1,7 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := pa_victara
